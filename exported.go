d037e25d303f3614701cbe5cec737a61b97afe7f
hongjy <hjy322@hotmail.com>
2017-05-03 14:41:30 +0800

    Initial commit

---
+++ exported.go
@@ -0,0 +1,110 @@
+package golog
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	std = &Logger{
+		Logger:*log.New(os.Stdout, "", log.Ldate | log.Ltime | log.Lshortfile),
+		level:TraceLevel,
+		calldepth: 1}
+)
+
+func Level() int {
+	return std.Level()
+}
+
+func SetLevel(level int) {
+	std.SetLevel(level)
+}
+
+func SetLevelString(levelString string) {
+	std.SetLevelString(levelString)
+}
+
+func Tracef(format string, v ...interface{}) {
+	std.Tracef(format, v...)
+}
+
+func Trace(v ...interface{}) {
+	std.Trace(v...)
+}
+
+func Traceln(v ...interface{}) {
+	std.Traceln(v...)
+}
+
+func Debugf(format string, v ...interface{}) {
+	std.Debugf(format, v...)
+}
+
+func Debug(v ...interface{}) {
+	std.Debug(v...)
+}
+
+func Debugln(v ...interface{}) {
+	std.Debugln(v...)
+}
+
+func Infof(format string, v ...interface{}) {
+	std.Infof(format, v...)
+}
+
+func Info(v ...interface{}) {
+	std.Info(v...)
+}
+
+func Infoln(v ...interface{}) {
+	std.Infoln(v...)
+}
+
+func Warnf(format string, v ...interface{}) {
+	std.Warnf(format, v...)
+}
+
+func Warn(v ...interface{}) {
+	std.Warn(v...)
+}
+
+func Warnln(v ...interface{}) {
+	std.Warnln(v...)
+}
+
+func Errorf(format string, v ...interface{}) {
+	std.Errorf(format, v...)
+}
+
+func Error(v ...interface{}) {
+	std.Error(v...)
+}
+
+func Errorln(v ...interface{}) {
+	std.Errorln(v...)
+}
+
+func Fatalf(format string, v ...interface{}) {
+	std.Fatalf(format, v...)
+}
+
+func Fatal(v ...interface{}) {
+	std.Fatal(v...)
+}
+
+func Fatalln(v ...interface{}) {
+	std.Fatalln(v...)
+}
+
+func Panicf(format string, v ...interface{}) {
+	std.Panicf(format, v...)
+}
+
+func Panic(v ...interface{}) {
+	std.Panic(v...)
+}
+
+func Panicln(v ...interface{}) {
+	std.Panicln(v...)
+}
+
---
+++ logger.go
@@ -0,0 +1,190 @@
+package golog
+
+import (
+	"log"
+	"fmt"
+	"os"
+	"sync"
+	"strings"
+)
+
+const (
+	TraceLevel = iota
+	DebugLevel
+	InfoLevel
+	WarningLevel
+	ErrorLevel
+	FatalLevel
+	PanicLevel
+)
+
+var (
+	LevelString = []string{"[TRACE] ", "[DEBUG] ", "[INFO] ", "[WARN] ", "[ERROR] ", "[FATAL] ", "[PANIC] "}
+)
+
+type Logger struct {
+	log.Logger
+	mu     sync.Mutex // ensures atomic writes; protects the following fields
+	level int
+	calldepth int
+}
+
+func (l *Logger) Level() int {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	return l.level
+}
+
+func (l *Logger) SetLevel(level int) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.level = level
+}
+
+func (l *Logger) SetLevelString(levelString string) {
+	switch strings.ToLower(levelString) {
+	case "trace":
+		l.SetLevel(TraceLevel)
+	case "debug":
+		l.SetLevel(DebugLevel)
+	case "info":
+		l.SetLevel(InfoLevel)
+	case "warn":
+		l.SetLevel(WarningLevel)
+	case "error":
+		l.SetLevel(ErrorLevel)
+	case "fatal":
+		l.SetLevel(FatalLevel)
+	case "panic":
+		l.SetLevel(PanicLevel)
+	}
+}
+
+func (l *Logger) Logf(level int, format string, v ...interface{}) {
+	if l.level > level {
+		return
+	}
+
+	s := fmt.Sprintf(format, v...)
+	l.Output(3 + l.calldepth, LevelString[level] + s)
+
+	if level == PanicLevel {
+		panic(s)
+	} else if level == FatalLevel {
+		os.Exit(1)
+	}
+}
+
+func (l *Logger) Log(level int, v ...interface{}) {
+	if l.level > level {
+		return
+	}
+
+	s := fmt.Sprint(v...)
+	l.Output(3 + l.calldepth, LevelString[level] + s)
+
+	if level == PanicLevel {
+		panic(s)
+	} else if level == FatalLevel {
+		os.Exit(1)
+	}
+}
+
+func (l *Logger) Logln(level int, v ...interface{}) {
+	if l.level > level {
+		return
+	}
+
+	s := fmt.Sprintln(v...)
+	l.Output(3 + l.calldepth, LevelString[level] + s)
+
+	if level == PanicLevel {
+		panic(s)
+	} else if level == FatalLevel {
+		os.Exit(1)
+	}
+}
+
+func (l *Logger) Tracef(format string, v ...interface{}) {
+	l.Logf(TraceLevel, format, v...)
+}
+
+func (l *Logger) Trace(v ...interface{}) {
+	l.Log(TraceLevel, v...)
+}
+
+func (l *Logger) Traceln(v ...interface{}) {
+	l.Logln(TraceLevel, v...)
+}
+
+func (l *Logger) Debugf(format string, v ...interface{}) {
+	l.Logf(DebugLevel, format, v...)
+}
+
+func (l *Logger) Debug(v ...interface{}) {
+	l.Log(DebugLevel, v...)
+}
+
+func (l *Logger) Debugln(v ...interface{}) {
+	l.Logln(DebugLevel, v...)
+}
+
+func (l *Logger) Infof(format string, v ...interface{}) {
+	l.Logf(InfoLevel, format, v...)
+}
+
+func (l *Logger) Info(v ...interface{}) {
+	l.Log(InfoLevel, v...)
+}
+
+func (l *Logger) Infoln(v ...interface{}) {
+	l.Logln(InfoLevel, v...)
+}
+
+func (l *Logger) Warnf(format string, v ...interface{}) {
+	l.Logf(WarningLevel, format, v...)
+}
+
+func (l *Logger) Warn(v ...interface{}) {
+	l.Log(WarningLevel, v...)
+}
+
+func (l *Logger) Warnln(v ...interface{}) {
+	l.Logln(WarningLevel, v...)
+}
+
+func (l *Logger) Errorf(format string, v ...interface{}) {
+	l.Logf(ErrorLevel, format, v...)
+}
+
+func (l *Logger) Error(v ...interface{}) {
+	l.Log(ErrorLevel, v...)
+}
+
+func (l *Logger) Errorln(v ...interface{}) {
+	l.Logln(ErrorLevel, v...)
+}
+
+func (l *Logger) Fatalf(format string, v ...interface{}) {
+	l.Logf(FatalLevel, format, v...)
+}
+
+func (l *Logger) Fatal(v ...interface{}) {
+	l.Log(FatalLevel, v...)
+}
+
+func (l *Logger) Fatalln(v ...interface{}) {
+	l.Logln(FatalLevel, v...)
+}
+
+func (l *Logger) Panicf(format string, v ...interface{}) {
+	l.Logf(PanicLevel, format, v...)
+}
+
+func (l *Logger) Panic(v ...interface{}) {
+	l.Log(PanicLevel, v...)
+}
+
+func (l *Logger) Panicln(v ...interface{}) {
+	l.Logln(PanicLevel, v...)
+}
